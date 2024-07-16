96d54241393f8e15c1c4637ff356365f14fc47e1
Matias Camiletti <matias.camiletti@gmail.com>
2024-07-15 22:29:54 -0300

    Create layout library.

---
+++ projects/mckit/layout/src/lib/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-layout',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      layout works!
+    </p>
+  `,
+  styles: ``
+})
+export class LayoutComponent {
+
+}
