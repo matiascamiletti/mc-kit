d97edd4254770eea988eb6d2511ba1bd3b7805ef
Matias Camiletti <matias.camiletti@gmail.com>
2024-07-15 22:47:27 -0300

    Create first structure for basic layout.

--- projects/example/src/app/app.routes.ts
+++ projects/example/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BasicLayout } from '../../../mckit/layout/src/lib/layouts/basic-layout/basic-layout.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    component: BasicLayout,
+    children: [
+      {
+        path: '',
+        component: TestPageComponent,
+        title: 'Test page'
+      },
+    ]
+  },
+];
---
+++ projects/mckit/layout/src/lib/layouts/basic-layout/basic-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'mc-basic-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './basic-layout.component.html',
+  styleUrl: './basic-layout.component.scss'
+})
+export class BasicLayout {
+
+}
