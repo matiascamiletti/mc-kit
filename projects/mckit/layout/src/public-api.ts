8da44f2d79f9e79292f76a9cb3f60e0a32d3285c
Matias Camiletti <matias.camiletti@gmail.com>
2024-07-27 00:33:51 -0300

    Update versions.

--- projects/mckit/layout/src/public-api.ts
+++ projects/mckit/layout/src/public-api.ts
@@ -12,6 +12,7 @@ export * from './lib/services/topbar.service';
  * Components
  */
 export * from './lib/components/icon-toggle-sidebar-button/icon-toggle-sidebar-button.component';
+export * from './lib/components/subtitle-in-topbar/subtitle-in-topbar.component';
 
 /**
  * Layouts
