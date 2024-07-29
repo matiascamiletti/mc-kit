346a58204e457fe0c28a3e83da594d35a9da3544
Matias Camiletti <matias.camiletti@gmail.com>
2024-07-28 23:32:01 -0300

    Create simple page structure.

--- projects/example/src/app/pages/test-page/test-page.component.ts
+++ projects/example/src/app/pages/test-page/test-page.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MCTopbarService } from '../../../../../mckit/layout/src/public-api';
+import { MCSimplePage } from '../../../../../mckit/layout/src/lib/pages/simple-page/simple-page.component';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-test-page',
   standalone: true,
-  imports: [],
+  imports: [MCSimplePage],
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.scss'
 })
 export class TestPageComponent implements OnInit {
+
+  breadcrumb: MenuItem[] = [
+    { label: 'Home', routerLink: '/' },
+    { label: 'Test Page' }
+  ];
 
   constructor(
     protected topbarService: MCTopbarService
---
+++ projects/mckit/layout/src/lib/pages/simple-page/simple-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'mc-simple-page',
+  standalone: true,
+  imports: [CommonModule, CardModule, BreadcrumbModule],
+  templateUrl: './simple-page.component.html',
+  styleUrl: './simple-page.component.css'
+})
+export class MCSimplePage {
+  @Input() breadcrumb: MenuItem[] | undefined;
+  @Input() title: string | undefined;
+  @Input() subtitle: string | undefined;
+}
