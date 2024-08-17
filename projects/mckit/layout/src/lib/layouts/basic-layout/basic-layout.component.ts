52e91428bc179f9ae3cd04afe7adf9e5bba54e50
Matias Camiletti <matias.camiletti@gmail.com>
2024-08-17 16:24:54 -0300

    Close sidebar automatic in mobile.

--- projects/mckit/layout/src/lib/layouts/basic-layout/basic-layout.component.ts
+++ projects/mckit/layout/src/lib/layouts/basic-layout/basic-layout.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, WritableSignal } from '@angular/core';
+import { Component, HostListener, OnInit, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ID_SIDEBAR_MC_COMPONENT, MCSidebarService } from '../../services/sidebar.service';
 import { PrintServiceComponent } from '@mckit/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ID_TOPBAR_MC_COMPONENT } from '../../services/topbar.service';
 
+declare const window: any;
 
 @Component({
   selector: 'mc-basic-layout',
@@ -22,10 +23,22 @@ export class BasicLayout implements OnInit {
   isOpen?: WritableSignal<boolean>;
 
   constructor(
-    protected sidebarService: MCSidebarService
+    protected sidebarService: MCSidebarService,
   ) {}
 
   ngOnInit(): void {
     this.isOpen = this.sidebarService.isOpen;
+    this.configResize();
+  }
+
+  configResize() {
+    if (window.innerWidth < 768) {
+      this.isOpen?.set(false);
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.configResize();
   }
 }
