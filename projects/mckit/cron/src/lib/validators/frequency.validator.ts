42202b625e5162fbf95d54bbba47cecf192fcc55
Matias Camiletti <matias.camiletti@gmail.com>
2025-01-06 22:19:38 -0300

    Fix validator cron

--- projects/mckit/cron/src/lib/validators/frequency.validator.ts
+++ projects/mckit/cron/src/lib/validators/frequency.validator.ts
@@ -3,6 +3,10 @@ import { isValidCron } from "../helpers/frequency.helper";
 
 export function frequencyValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
+    if(control.value == undefined || control.value == ''){
+      return null;
+    }
+
     return isValidCron(control.value) ? null : { invalidFrequency: {value: control.value} };
   };
 }
