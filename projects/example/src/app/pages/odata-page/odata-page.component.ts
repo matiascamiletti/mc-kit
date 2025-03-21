3876f50feffe60f82703aaccfdafac8984f44403
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-21 19:01:35 -0300

    feat: add filter button component to OData page for enhanced filtering capabilities

--- projects/example/src/app/pages/odata-page/odata-page.component.ts
+++ projects/example/src/app/pages/odata-page/odata-page.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MCOdataPage } from '../../../../../mckit/odata/src/public-api';
 import { MenuItem } from 'primeng/api';
+import { MCConfigFilter, MCFilter, MCItemFilter } from '../../../../../mckit/filter/src/public-api';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-odata-page',
@@ -9,11 +11,90 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './odata-page.component.html',
   styleUrl: './odata-page.component.scss'
 })
-export class OdataPageComponent {
+export class OdataPageComponent implements OnInit {
 
   breadcrumb: MenuItem[] = [
       { label: 'Home', routerLink: '/' },
       { label: 'Users' }
   ];
 
+  filters = new MCConfigFilter();
+
+  ngOnInit(): void {
+    this.loadFilterConfig();
+  }
+
+  loadFilterConfig() {
+      this.filters.filters = [
+        MCFilter.text({
+          title: 'Name',
+          key: 'name'
+        }),
+        MCFilter.textQuickFilter({
+          title: 'Game #',
+          key: 'game_number',
+          options: [
+            { label: '1', value: '1' },
+            { label: '2', value: '2' },
+            { label: '3', value: '3' },
+            { label: '4', value: '4' },
+            { label: '5', value: '5' },
+            { label: '6', value: '6' },
+            { label: '7', value: '7' },
+            { label: '8', value: '8' },
+            { label: '9', value: '9' },
+              { label: '10', value: '10' }
+          ]
+        }),
+        MCFilter.selectQuickFilter({
+          title: 'Status',
+          key: 'status',
+          options: [
+            { label: 'In progress', value: 'In progress' },
+            { label: 'Completed', value: 'Completed' },
+            { label: 'Cancelled', value: 'Cancelled' },
+          ],
+          placeholder: 'Select a status'
+        }),
+        MCFilter.textQuickFilter({
+          title: 'Text Quick Filter Field',
+          key: 'field',
+          options: [{ label: 'Field La bombonera', value: 'Field La bombonera' }],
+        }),
+        MCFilter.autocomplete({
+          title: 'Autocomplete Field',
+          key: 'field',
+          filter: this.onFilterAutocomplete.bind(this),
+        }),
+        MCFilter.multiselect({
+          title: 'Multi Select Field',
+          key: 'field',
+          options: [
+            { label: 'Field La bombonera', value: 'Field La bombonera' },
+            { label: 'Mostaza', value: 'Mostaza' },
+            { label: 'La bombonera', value: 'La bombonera' },
+            { label: 'McDonalds', value: 'McDonalds' },
+          ],
+        }),
+        MCFilter.select({
+          title: 'Select Field',
+          key: 'field',
+          options: [
+            { label: 'Field La bombonera', value: 'Field La bombonera' },
+            { label: 'Mostaza', value: 'Mostaza' },
+            { label: 'La bombonera', value: 'La bombonera' },
+            { label: 'McDonalds', value: 'McDonalds' },
+          ],
+        }),
+      ];
+    }
+
+  onFilterAutocomplete(query: string): Observable<Array<MCItemFilter>> {
+      return of([
+        { label: 'Field La bombonera', value: 'Field La bombonera' },
+        { label: 'Mostaza', value: 'Mostaza' },
+        { label: 'La bombonera', value: 'La bombonera' },
+        { label: 'McDonalds', value: 'McDonalds' },
+      ]);
+    }
 }
--- projects/mckit/odata/src/lib/pages/odata-page/odata-page.component.ts
+++ projects/mckit/odata/src/lib/pages/odata-page/odata-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, viewChild } from '@angular/core';
+import { MCConfigFilter, MCFilterButton, MCResultFilter } from '@mckit/filter';
 import { MCPageHeadingComponent, MCSearchField } from '@mckit/layout-core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'mc-odata-page',
-  imports: [CommonModule, MCPageHeadingComponent, MCSearchField],
+  imports: [CommonModule, MCPageHeadingComponent, MCSearchField, MCFilterButton],
   templateUrl: './odata-page.component.html',
   styleUrl: './odata-page.component.css'
 })
@@ -20,7 +21,14 @@ export class MCOdataPage {
 
   searchField = viewChild(MCSearchField);
 
+  filters = input<MCConfigFilter>();
+
   onSearch(query: string) {
     this.searchField()?.stopLoading();
+  }
+
+  onFilter(filters: Array<MCResultFilter>) {
+    //console.log(filters);
+    //console.log(this.odataConverter.convert(filters));
   }
 }
