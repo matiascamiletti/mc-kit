2e3e8506ac8b9963b54647056c397d656b8a0c32
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-22 12:22:40 -0300

    feat: add TestService with unit tests

---
+++ projects/example/src/app/services/test.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { MCApiRestHttpService } from '@mckit/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService extends MCApiRestHttpService<any> {
+
+}
