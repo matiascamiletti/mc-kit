42897517c72d4ffaef06f9053ba86dff0b3f0faf
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-23 20:40:42 -0300

    feat: add event handling to MCFormComponent and implement form submission logic

--- projects/example/src/app/pages/form-page/form-page.component.ts
+++ projects/example/src/app/pages/form-page/form-page.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
-import { MCConfigForm, MCFormComponent } from '../../../../../mckit/form/src/public-api';
+import { Component, OnInit, signal, viewChild } from '@angular/core';
+import { MCConfigForm, MCEventForm, MCFormComponent } from '../../../../../mckit/form/src/public-api';
 import { IftaTextField } from '../../../../../mckit/form/src/lib/fields/ifta-text-field/ifta-text-field.component';
+import { SubmitButtonField } from '../../../../../mckit/form/src/lib/fields/submit-button-field/submit-button-field.component';
+import { Validators } from '@angular/forms';
+import { AnyNaptrRecord } from 'node:dns';
 
 @Component({
   selector: 'app-form-page',
@@ -11,17 +14,36 @@ import { IftaTextField } from '../../../../../mckit/form/src/lib/fields/ifta-tex
 })
 export class FormPageComponent implements OnInit {
 
+  formComponent = viewChild(MCFormComponent);
+
   formConfig = signal<MCConfigForm>(new MCConfigForm());
 
   ngOnInit(): void {
     this.loadForm();
   }
 
+  onSubmit(result: AnyNaptrRecord) {
+    setTimeout(() => {
+      this.formComponent()?.emitEvent(MCEventForm.init('stop-loading'));
+    }, 2000);
+  }
+
+  onEventForm(event: MCEventForm) {
+    console.log(event);
+
+    switch (event.key) {
+      case 'submit':
+        this.onSubmit(event.content);
+        break;
+    }
+  }
+
   loadForm() {
     let config = this.formConfig();
     config.fields = [
-      IftaTextField.init('firstname', 'Firstname'),
+      IftaTextField.init('firstname', 'Firstname', { validators: [Validators.required] }),
       IftaTextField.init('lastname', 'Lastname'),
+      SubmitButtonField.init('submit', 'Submit', { icon: 'pi pi-check' })
     ];
 
     this.formConfig.set(config);
