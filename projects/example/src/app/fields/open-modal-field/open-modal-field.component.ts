1a8286e4dcf65f3c94cd376aa211d3c803c673bd
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-24 09:37:53 -0300

    feat: add OpenModalField component for enhanced modal functionality in forms

---
+++ projects/example/src/app/fields/open-modal-field/open-modal-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MCFieldComponent } from '../../../../../mckit/form/src/public-api';
+
+@Component({
+  selector: 'app-open-modal-field',
+  imports: [],
+  templateUrl: './open-modal-field.component.html',
+  styleUrl: './open-modal-field.component.scss'
+})
+export class OpenModalFieldComponent extends MCFieldComponent {
+
+}
--- projects/example/src/app/pages/form-page/form-page.component.ts
+++ projects/example/src/app/pages/form-page/form-page.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal, viewChild } from '@angular/core';
-import { MCConfigForm, MCEventForm, MCFormComponent, RowField } from '../../../../../mckit/form/src/public-api';
+import { MCConfigForm, MCEventForm, MCField, MCFormComponent, RowField } from '../../../../../mckit/form/src/public-api';
 import { IftaTextField } from '../../../../../mckit/form/src/lib/fields/ifta-text-field/ifta-text-field.component';
 import { SubmitButtonField } from '../../../../../mckit/form/src/lib/fields/submit-button-field/submit-button-field.component';
 import { Validators } from '@angular/forms';
+import { OpenModalFieldComponent } from '../../fields/open-modal-field/open-modal-field.component';
 
 @Component({
   selector: 'app-form-page',
@@ -48,6 +49,7 @@ export class FormPageComponent implements OnInit {
     config.fields = [
       IftaTextField.init('firstname', 'Firstname', { validators: [Validators.required] }),
       IftaTextField.init('lastname', 'Lastname'),
+      MCField.initCustom('open-modal', OpenModalFieldComponent, { }),
       RowField.init([
         IftaTextField.init('email', 'Email'),
         IftaTextField.init('phone', 'Phone'),
