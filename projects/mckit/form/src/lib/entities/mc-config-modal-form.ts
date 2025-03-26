f53b5bed967b3fc5fd5072404aece0108368ab30
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-26 17:22:49 -0300

    feat: add MCConfigModalForm class extending MCConfigForm with additional properties

---
+++ projects/mckit/form/src/lib/entities/mc-config-modal-form.ts
@@ -0,0 +1,8 @@
+import { MCConfigForm } from "./mc-config-form";
+
+export class MCConfigModalForm extends MCConfigForm {
+  title?: string;
+  position?: string;
+  styleClass?: string;
+  style?: any;
+}
