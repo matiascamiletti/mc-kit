773a3cfba4310d0c82e3c2571c972c8356215d8c
Matias Camiletti <matias.camiletti@gmail.com>
2025-04-28 08:18:12 -0300

    Merge pull request #12 from snoli2001/CU-86b4ra494
    CU-86b4ra494 | feat: Go to first on filter

--- projects/mckit/odata/src/lib/components/odata-signal-table.component.ts
+++ projects/mckit/odata/src/lib/components/odata-signal-table.component.ts
@@ -65,6 +65,7 @@ export abstract class MCOdataSignalTableComponent<T extends { id?: any }> implem
 
   filter(event: TableFilterEvent) {
     this.data.filters.setTableFilters(event);
+    this.data.setPage(1, this.data.top || 50);
     this.loadItems();
   }
 
