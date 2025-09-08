4769be0ac7d6e0446bc7febae6381218b11871b4
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-07 23:26:26 -0300

    feat: add image_url property to MCTenant entity

--- projects/mckit/tenant/src/lib/entities/mc_tenant.ts
+++ projects/mckit/tenant/src/lib/entities/mc_tenant.ts
@@ -2,6 +2,7 @@ export class MCTenant {
     id?: string|number;
     name?: string;
     description?: string;
+    image_url?: string;
     status?: number;
     created_at?: string;
     updated_at?: string;
