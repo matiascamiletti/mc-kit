8b6e61e674fa0b8bac2c886de45de9ea9722f98e
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-07 23:23:02 -0300

    feat: update tenant service to inject authentication configuration and enhance package.json dependencies

--- projects/mckit/tenant/src/lib/services/tenant.service.ts
+++ projects/mckit/tenant/src/lib/services/tenant.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { MCApiRestHttpService } from '@mckit/core';
 import { MCTenant } from '../entities/mc_tenant';
+import { MC_AUTH_CONFIG } from '@mckit/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MCTenantService extends MCApiRestHttpService<MCTenant> {
   override pathModel = '/tenants';
+
+  config = inject(MC_AUTH_CONFIG);
+
+  constructor() {
+    super();
+    this.baseUrl = this.config.baseUrl;
+  }
 }
