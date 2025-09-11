ad07b0a04e67000a40b6eebcf7dba37b941af184
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-10 22:12:23 -0300

    feat: enhance MCTenantService with storage management for current tenant

--- projects/mckit/tenant/src/lib/services/tenant.service.ts
+++ projects/mckit/tenant/src/lib/services/tenant.service.ts
@@ -2,17 +2,62 @@ import { inject, Injectable } from '@angular/core';
 import { MCApiRestHttpService } from '@mckit/core';
 import { MCTenant } from '../entities/mc_tenant';
 import { MC_AUTH_CONFIG } from '@mckit/auth';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { StorageMap } from '@ngx-pwa/local-storage';
+
+export const MC_TENANT_KEY_STORAGE = 'mc.tenant.storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MCTenantService extends MCApiRestHttpService<MCTenant> {
-  override pathModel = '/tenants';
 
   config = inject(MC_AUTH_CONFIG);
+  storage = inject(StorageMap);
+
+  override pathModel = '/tenants';
+
+  current = new BehaviorSubject<MCTenant|undefined>(undefined);
 
   constructor() {
     super();
     this.baseUrl = this.config.baseUrl;
+    this.initialize();
+  }
+
+  getCurrent(): Observable<MCTenant|undefined> {
+    return this.current.asObservable();
+  }
+
+  setCurrent(tenant: MCTenant|undefined) {
+    this.current.next(tenant);
+    if(tenant == undefined){
+      this.removeCurrentFromStorage().subscribe();
+    } else {
+      this.saveCurrentInStorage(tenant).subscribe();
+    }
+  }
+
+  saveCurrentInStorage(tenant: MCTenant): Observable<undefined> {
+    return this.storage.set(MC_TENANT_KEY_STORAGE, JSON.stringify(tenant), { type: 'string' });
+  }
+
+  getCurrentFromStorage(): Observable<MCTenant|undefined> {
+    return this.storage.get(MC_TENANT_KEY_STORAGE, { type: 'string' })
+    .pipe(map(data => {
+      if(data == undefined||data == ''){
+        return undefined;
+      }
+      return JSON.parse(data);
+    }));
+  }
+
+  removeCurrentFromStorage(): Observable<undefined> {
+    return this.storage.delete(MC_TENANT_KEY_STORAGE);
+  }
+
+  initialize() {
+    this.getCurrentFromStorage()
+    .subscribe(tenant => this.setCurrent(tenant));
   }
 }
