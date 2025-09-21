31b225a513719d3bbd130addcbf325bf3da974d8
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-21 08:18:53 -0300

    feat: implement tenant loading logic and enhance tenant menu component

--- projects/mckit/tenant/src/lib/components/tenant-menu/tenant-menu.component.ts
+++ projects/mckit/tenant/src/lib/components/tenant-menu/tenant-menu.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { MCComponent, MCCoreComponent } from '@mckit/core';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { ImageModule } from 'primeng/image';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { MCTenantService } from '../../services/tenant.service';
+import { MCTenant } from '../../entities/mc_tenant';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'mc-tenant-menu',
@@ -13,10 +16,21 @@ import { SplitButtonModule } from 'primeng/splitbutton';
   templateUrl: './tenant-menu.component.html',
   styleUrl: './tenant-menu.component.scss'
 })
-export class TenantMenuComponent extends MCCoreComponent {
+export class TenantMenuComponent extends MCCoreComponent implements OnDestroy {
+
+  tenantService = inject(MCTenantService);
+
+  current = signal<MCTenant | undefined>(undefined);
+  currentSubscription?: Subscription;
+
+  isLoading = signal(true);
+
   items: MenuItem[] | undefined;
 
   ngOnInit() {
+    this.loadCurrent();
+    this.loadAllTenants();
+
       this.items = [
           {
               label: 'Tenant 1',
@@ -31,6 +45,23 @@ export class TenantMenuComponent extends MCCoreComponent {
               icon: 'pi pi-external-link'
           }
       ];
+  }
+
+  ngOnDestroy() {
+    this.currentSubscription?.unsubscribe();
+  }
+
+  loadAllTenants() {
+    this.tenantService.list('')
+  }
+
+  loadCurrent() {
+    this.currentSubscription = this.tenantService.getCurrent()
+    .pipe(
+      tap((tenant: MCTenant | undefined) => this.current.set(tenant)),
+      tap(() => this.isLoading.set(false))
+    )
+    .subscribe();
   }
 }
 
