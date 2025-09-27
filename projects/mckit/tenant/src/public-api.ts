98571f70faf45f42a2f3565b0f63f55cd964402e
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-27 20:13:21 -0300

    feat: reorganize public API surface for better structure and clarity

--- projects/mckit/tenant/src/public-api.ts
+++ projects/mckit/tenant/src/public-api.ts
@@ -17,3 +17,13 @@ export * from './lib/services/tenant.service';
  * Interceptors
  */
 export * from './lib/interceptors/tenant.interceptor';
+
+/**
+ * Resolvers
+ */
+export * from './lib/resolvers/tenant.resolver';
+
+/**
+ * Components
+ */
+export * from './lib/components/tenant-menu/tenant-menu.component';
