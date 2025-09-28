e53dfbce433d13a761d936025ff3281eedba72a3
Matias Camiletti <matias.camiletti@gmail.com>
2025-09-28 19:14:14 -0300

    refactor: update tenantResolver to use filter and take for improved tenant retrieval

--- projects/mckit/tenant/src/lib/resolvers/tenant.resolver.ts
+++ projects/mckit/tenant/src/lib/resolvers/tenant.resolver.ts
@@ -2,9 +2,12 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { MCTenantService } from '../services/tenant.service';
 import { MCTenant } from '../entities/mc_tenant';
-import { takeWhile } from 'rxjs';
+import { filter, take, takeWhile } from 'rxjs';
 
 export const tenantResolver: ResolveFn<MCTenant> = (route, state) => {
   const tenantService = inject(MCTenantService);
-  return tenantService.getCurrent().pipe(takeWhile((tenant) => tenant != undefined));
+  return tenantService.getCurrent().pipe(
+    filter((tenant) => tenant != undefined),
+    take(1)
+  );
 };
