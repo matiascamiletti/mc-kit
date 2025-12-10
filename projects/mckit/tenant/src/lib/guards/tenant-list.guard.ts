f8b362db20f1a204a7eec7e29aeadf6a164acff3
Matias Camiletti <matias.camiletti@gmail.com>
2025-12-10 20:50:27 -0300

    feat: tenant list guard now checks for current tenant before listing and bumps package version

--- projects/mckit/tenant/src/lib/guards/tenant-list.guard.ts
+++ projects/mckit/tenant/src/lib/guards/tenant-list.guard.ts
@@ -1,20 +1,28 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { MCTenantService } from '../services/tenant.service';
-import { map, take } from 'rxjs';
+import { map, of, switchMap, take } from 'rxjs';
+import { MCTenant } from '@mckit/tenant';
 
 export const tenantListGuard: CanActivateFn = (route, state) => {
     const tenantService = inject(MCTenantService);
     const router = inject(Router);
 
-    return tenantService.list().pipe(
-        take(1),
-        map((tenants) => {
-            if (tenants.data.length > 0) {
-                tenantService.setCurrent(tenants.data[0]);
-                return true;
+    return tenantService.getCurrent().pipe(
+        switchMap((tenant: MCTenant | undefined) => {
+            if (tenant != undefined) {
+                return of(true);
             }
-            return router.createUrlTree(['/no-tenant']);
+            return tenantService.list().pipe(
+                take(1),
+                map((tenants) => {
+                    if (tenants.data.length > 0) {
+                        tenantService.setCurrent(tenants.data[0]);
+                        return true;
+                    }
+                    return router.createUrlTree(['/no-tenant']);
+                })
+            );
         })
     );
 };
