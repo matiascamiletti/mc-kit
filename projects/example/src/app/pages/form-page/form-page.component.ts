eb01e15bf2498a1d807a41c1bf174e1a5be7519e
Matias Camiletti <matias.camiletti@gmail.com>
2026-03-13 16:22:36 -0300

    feat: add color field in example.

--- projects/example/src/app/pages/form-page/form-page.component.ts
+++ projects/example/src/app/pages/form-page/form-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal, viewChild } from '@angular/core';
-import { ArrayField, ArrayIftaTextField, ConditionalField, DividerField, IftaDateField, IftaSelectField, IftaSelectObsField, MCConfigForm, MCEventForm, MCField, MCForm, RowField, IftaPasswordField } from '../../../../../mckit/form/src/public-api';
+import { ArrayField, ArrayIftaTextField, ConditionalField, DividerField, IftaDateField, IftaSelectField, IftaSelectObsField, MCConfigForm, MCEventForm, MCField, MCForm, RowField, IftaPasswordField, ColorField } from '../../../../../mckit/form/src/public-api';
 import { IftaTextField } from '../../../../../mckit/form/src/lib/fields/ifta-text-field/ifta-text-field.component';
 import { SubmitButtonField } from '../../../../../mckit/form/src/lib/fields/submit-button-field/submit-button-field.component';
 import { Validators } from '@angular/forms';
@@ -109,6 +109,7 @@ export class FormPageComponent implements OnInit {
       DividerField.init('Events'),
       ArrayIftaTextField.init('events', { labelAddButton: 'Add Event', labelTitlePanel: 'Events' }),
       IftaDateField.init('date', 'Date', { date_format: 'dd-mm-yy' }),
+      ColorField.init('color', { label: 'Color Hex' }),
       SubmitButtonField.init('submit', 'Submit', { icon: 'pi pi-check' })
     ];
 
