e272ab5561a1ccda5cdd5861289c645992b48e66
Matias Camiletti <matias.camiletti@gmail.com>
2026-03-13 15:50:54 -0300

    feat: add checkbox in odata page.

--- projects/example/src/app/pages/odata-page/odata-page.component.ts
+++ projects/example/src/app/pages/odata-page/odata-page.component.ts
@@ -7,10 +7,11 @@ import { Observable, of } from 'rxjs';
 import { ButtonModule } from 'primeng/button';
 import { MCActionsColumn, MCTdTemplateDirective, MCThTemplateDirective } from '@mckit/table';
 import { TestService } from '../../services/test.service';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-odata-page',
-  imports: [CommonModule, MCOdataPage, ButtonModule, MCThTemplateDirective, MCTdTemplateDirective, MCActionsColumn, MCRightHeaderTemplateDirective],
+  imports: [CommonModule, MCOdataPage, ButtonModule, MCThTemplateDirective, MCTdTemplateDirective, MCActionsColumn, MCRightHeaderTemplateDirective, CheckboxModule],
   templateUrl: './odata-page.component.html',
   styleUrl: './odata-page.component.scss'
 })
@@ -19,8 +20,8 @@ export class OdataPageComponent implements OnInit {
   odataPage = viewChild(MCOdataPage);
 
   breadcrumb: MenuItem[] = [
-      { label: 'Home', routerLink: '/' },
-      { label: 'Users' }
+    { label: 'Home', routerLink: '/' },
+    { label: 'Users' }
   ];
 
   testService = inject(TestService);
@@ -28,6 +29,7 @@ export class OdataPageComponent implements OnInit {
   filters = new MCConfigFilter();
 
   columns = [
+    { field: 'check', title: 'Check', isShow: true },
     { field: 'name', title: 'Name', isSortable: true, isSortDefault: true, isSortDescDefault: false },
     { field: 'game_number', title: 'Game #' },
     { field: 'status', title: 'Status', isShow: true },
@@ -50,76 +52,76 @@ export class OdataPageComponent implements OnInit {
   }
 
   loadFilterConfig() {
-      this.filters.filters = [
-        MCFilter.text({
-          title: 'Name',
-          key: 'name'
-        }),
-        MCFilter.textQuickFilter({
-          title: 'Game #',
-          key: 'game_number',
-          options: [
-            { label: '1', value: '1' },
-            { label: '2', value: '2' },
-            { label: '3', value: '3' },
-            { label: '4', value: '4' },
-            { label: '5', value: '5' },
-            { label: '6', value: '6' },
-            { label: '7', value: '7' },
-            { label: '8', value: '8' },
-            { label: '9', value: '9' },
-              { label: '10', value: '10' }
-          ]
-        }),
-        MCFilter.selectQuickFilter({
-          title: 'Status',
-          key: 'status',
-          options: [
-            { label: 'In progress', value: 'In progress' },
-            { label: 'Completed', value: 'Completed' },
-            { label: 'Cancelled', value: 'Cancelled' },
-          ],
-          placeholder: 'Select a status'
-        }),
-        MCFilter.textQuickFilter({
-          title: 'Text Quick Filter Field',
-          key: 'field',
-          options: [{ label: 'Field La bombonera', value: 'Field La bombonera' }],
-        }),
-        MCFilter.autocomplete({
-          title: 'Autocomplete Field',
-          key: 'field',
-          filter: this.onFilterAutocomplete.bind(this),
-        }),
-        MCFilter.multiselect({
-          title: 'Multi Select Field',
-          key: 'field',
-          options: [
-            { label: 'Field La bombonera', value: 'Field La bombonera' },
-            { label: 'Mostaza', value: 'Mostaza' },
-            { label: 'La bombonera', value: 'La bombonera' },
-            { label: 'McDonalds', value: 'McDonalds' },
-          ],
-        }),
-        MCFilter.select({
-          title: 'Select Field',
-          key: 'field',
-          options: [
-            { label: 'Field La bombonera', value: 'Field La bombonera' },
-            { label: 'Mostaza', value: 'Mostaza' },
-            { label: 'La bombonera', value: 'La bombonera' },
-            { label: 'McDonalds', value: 'McDonalds' },
-          ],
-        }),
-      ];
-    }
+    this.filters.filters = [
+      MCFilter.text({
+        title: 'Name',
+        key: 'name'
+      }),
+      MCFilter.textQuickFilter({
+        title: 'Game #',
+        key: 'game_number',
+        options: [
+          { label: '1', value: '1' },
+          { label: '2', value: '2' },
+          { label: '3', value: '3' },
+          { label: '4', value: '4' },
+          { label: '5', value: '5' },
+          { label: '6', value: '6' },
+          { label: '7', value: '7' },
+          { label: '8', value: '8' },
+          { label: '9', value: '9' },
+          { label: '10', value: '10' }
+        ]
+      }),
+      MCFilter.selectQuickFilter({
+        title: 'Status',
+        key: 'status',
+        options: [
+          { label: 'In progress', value: 'In progress' },
+          { label: 'Completed', value: 'Completed' },
+          { label: 'Cancelled', value: 'Cancelled' },
+        ],
+        placeholder: 'Select a status'
+      }),
+      MCFilter.textQuickFilter({
+        title: 'Text Quick Filter Field',
+        key: 'field',
+        options: [{ label: 'Field La bombonera', value: 'Field La bombonera' }],
+      }),
+      MCFilter.autocomplete({
+        title: 'Autocomplete Field',
+        key: 'field',
+        filter: this.onFilterAutocomplete.bind(this),
+      }),
+      MCFilter.multiselect({
+        title: 'Multi Select Field',
+        key: 'field',
+        options: [
+          { label: 'Field La bombonera', value: 'Field La bombonera' },
+          { label: 'Mostaza', value: 'Mostaza' },
+          { label: 'La bombonera', value: 'La bombonera' },
+          { label: 'McDonalds', value: 'McDonalds' },
+        ],
+      }),
+      MCFilter.select({
+        title: 'Select Field',
+        key: 'field',
+        options: [
+          { label: 'Field La bombonera', value: 'Field La bombonera' },
+          { label: 'Mostaza', value: 'Mostaza' },
+          { label: 'La bombonera', value: 'La bombonera' },
+          { label: 'McDonalds', value: 'McDonalds' },
+        ],
+      }),
+    ];
+  }
 
   onFilterAutocomplete(query: string): Observable<Array<MCItemFilter>> {
-      return of([
-        { label: 'Field La bombonera', value: 'Field La bombonera' },
-        { label: 'Mostaza', value: 'Mostaza' },
-        { label: 'La bombonera', value: 'La bombonera' },
-        { label: 'McDonalds', value: 'McDonalds' },
-      ]);
-    }
+    return of([
+      { label: 'Field La bombonera', value: 'Field La bombonera' },
+      { label: 'Mostaza', value: 'Mostaza' },
+      { label: 'La bombonera', value: 'La bombonera' },
+      { label: 'McDonalds', value: 'McDonalds' },
+    ]);
+  }
 }
