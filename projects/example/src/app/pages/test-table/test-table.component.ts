133f9d80df5dc256ad62d0e00296a96d95a1be72
Matias Camiletti <matias.camiletti@gmail.com>
2026-04-17 23:26:51 -0300

    fix: convert to signal filter config.

--- projects/example/src/app/pages/test-table/test-table.component.ts
+++ projects/example/src/app/pages/test-table/test-table.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, viewChild } from '@angular/core';
+import { Component, OnInit, signal, viewChild } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
@@ -7,23 +7,16 @@ import { ButtonModule } from 'primeng/button';
 import { TestTableService, Product } from './test-table.service';
 import { inject } from '@angular/core';
 
-import {
-  MCFilterButton,
-  MCFilter,
-  MCConfigFilter,
-  MCResultFilter,
-} from '@mckit/filter';
-
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { MCOdataTableComponent } from '@mckit/odata';
-import { MCFilterOdataConverterService } from '@mckit/filter';
 import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputIconModule } from 'primeng/inputicon';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MCConfigFilter, MCFilter, MCFilterButton, MCFilterOdataConverterService, MCResultFilter } from '../../../../../mckit/filter/src/public-api';
 
 @Component({
   selector: 'app-test-table',
@@ -48,14 +41,14 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
 })
 export class TestTableComponent
   extends MCOdataTableComponent<Product>
-  implements OnInit
-{
+  implements OnInit {
   override httpService = inject(TestTableService);
   private odataConverter = inject(MCFilterOdataConverterService);
 
   override tableKey = 'test-table-list';
   filterButton = viewChild<MCFilterButton>('McFilterButton');
-  filterConfig = new MCConfigFilter();
+
+  filterConfig = signal<MCConfigFilter>(new MCConfigFilter());
 
   override columns = [
     { field: 'id', title: 'ID', isShow: true },
@@ -72,45 +65,48 @@ export class TestTableComponent
   override ngOnInit() {
     this.data.top = 10;
     this.data.orderBy = 'createdAt desc';
-    super.ngOnInit();
     this.loadFilterConfig();
+    super.ngOnInit();
   }
 
   loadFilterConfig() {
-    this.filterConfig.filters = [
-      MCFilter.text({
-        title: 'Name',
-        key: 'name',
-      }),
-      MCFilter.multiselect({
-        title: 'Category',
-        key: 'category',
-        options: [
-          { label: 'Electronics', value: 'Electronics' },
-          { label: 'Clothing', value: 'Clothing' },
-          { label: 'Food', value: 'Food' },
-        ],
-      }),
-      MCFilter.multiselect({
-        title: 'Rating',
-        key: 'rating',
-        options: [
-          { label: '4.5+ Stars', value: '4.5' },
-          { label: '4.0+ Stars', value: '4.0' },
-          { label: '3.5+ Stars', value: '3.5' },
-        ],
-      }),
-      MCFilter.multiselectAutocomplete({
-        title: 'Categories Search',
-        key: 'category',
-        placeholder: 'Search and select categories',
-        filter: (query: string) => this.httpService.getCategories(query),
-        maxSelectedLabels: 1,
-      }),
-    ];
+    this.filterConfig.set({
+      filters: [
+        MCFilter.text({
+          title: 'Name',
+          key: 'name',
+        }),
+        MCFilter.multiselect({
+          title: 'Category',
+          key: 'category',
+          options: [
+            { label: 'Electronics', value: 'Electronics' },
+            { label: 'Clothing', value: 'Clothing' },
+            { label: 'Food', value: 'Food' },
+          ],
+        }),
+        MCFilter.multiselect({
+          title: 'Rating',
+          key: 'rating',
+          options: [
+            { label: '4.5+ Stars', value: '4.5' },
+            { label: '4.0+ Stars', value: '4.0' },
+            { label: '3.5+ Stars', value: '3.5' },
+          ],
+        }),
+        MCFilter.multiselectAutocomplete({
+          title: 'Categories Search',
+          key: 'category',
+          placeholder: 'Search and select categories',
+          filter: (query: string) => this.httpService.getCategories(query),
+          maxSelectedLabels: 1,
+        }),
+      ]
+    });
   }
 
   onFilter(filters: Array<MCResultFilter>) {
+    console.log('filters', filters);
     let filterOdata = this.odataConverter.convert(filters);
     this.data.filters.cleanPostpend();
     this.data.filters.setPostpend(filterOdata);
