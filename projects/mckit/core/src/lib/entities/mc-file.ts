96d92a53d173edbddb909b9455a5cc90f16352bd
Matias Camiletti <matias.camiletti@gmail.com>
2026-07-05 14:22:59 -0300

    feat: add fields in mc file entity.

--- projects/mckit/core/src/lib/entities/mc-file.ts
+++ projects/mckit/core/src/lib/entities/mc-file.ts
@@ -1,6 +1,22 @@
+export enum MCFileType {
+    Folder = 0,
+    File = 1,
+}
+
+export enum MCFileVisibility {
+    Private = 0,
+    Public = 1,
+}
+
 export class MCFile {
     id?: number | string;
     name?: string;
     url?: string;
     size?: number;
+    parent_id?: number | string;
+    type?: MCFileType;
+    visibility?: MCFileVisibility;
+    mime_type?: string;
+    sort?: number;
+    created_at?: string;
 }
